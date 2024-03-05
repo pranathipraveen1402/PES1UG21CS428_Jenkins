8913b2a79e31d91d8339f0afe2d315d76549aae7
pranathipraveen1402 <pranathipraveen03@gmail.com>
2024-03-05 07:56:38 +0530

    changed cpp file 2

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "I, PES1UG21CS428, using Ngrox, have successfully built and run\n";
+  cout << "This must execute";
   return 0;
 }
