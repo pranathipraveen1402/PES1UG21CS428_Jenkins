32efee2f9fd895d791c895e509c897b30c220abf
pranathipraveen1402 <pranathipraveen03@gmail.com>
2024-03-05 15:47:29 +0530

    sending main.cpp -2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
-
-int main(void) {
-  cout << "Pipelines are better than freestyle jobs, you can write a lot of complex tasks using pipelines
-when compared to Freestyle jobs\n";
-  cout << "Pipeline defines your entire build process, which typically includes stages for building an
-application, testing it and then delivering it.\n";
-  cout << "Here we are going to create a basic Jenkins pipeline.\n";
  
-
-  return 0;
+// Driver Code
+int main()
+{
+    // Print standard output
+    // on the screen
+    cout << "DOING PIPELINE PES1UG21CS428";
+ 
+    return 0;
 }
